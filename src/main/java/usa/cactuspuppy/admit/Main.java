0911e66649436a2569da7a03843309628a4894df
CactusPuppy <coby.zhong@gmail.com>
2019-06-18 21:00:01 -0700

    Initial setup

---
+++ src/main/java/usa/cactuspuppy/admit/Main.java
@@ -0,0 +1,17 @@
+package usa.cactuspuppy.admit;
+
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.java.annotation.plugin.*;
+import org.bukkit.plugin.java.annotation.plugin.author.Author;
+
+public class Main extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
